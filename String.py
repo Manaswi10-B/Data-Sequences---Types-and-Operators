6ddf302e89b218f1035eac89ee8fb492a6a040ff
Manaswi <bhosalem0000@gmail.com>
2026-06-02 14:28:04 +0530

    Added string operations program

---
+++ String.py
@@ -0,0 +1,51 @@
+#Creating a string
+name="Manaswi"
+print("The name is:",name)
+
+#Accesing Characters in string
+name="Manaswi"
+print("First Character:",name[0])
+print("Secong Character:",name[6])
+
+#Slicing
+name="Manaswi"
+print(name[0:4])
+print(name[0:3])
+
+#Index
+name="Manaswi"
+print("The index number:",name[4])
+
+#Length of string
+name="Manaswi"
+print(len(name))
+
+#Updating string
+name="Manaswi Bhosale"
+print("Updated String:",name)
+
+#Concatenating Strings
+name1="Manaswi"
+name2="Bhosale"
+concat_str= name1 + name2
+print("Concatenated String:",concat_str)
+
+#Repeating String
+name="Manaswi"
+print("Repeated String:",name*3)
+
+#Lower()
+name="MANASWI"
+print("Lower case:",name.lower())
+
+#Upper()
+name="manaswi"
+print("Upper case:",name.upper())
+
+#Capitalize()
+name="manaswi"
+print("Capitalized:",name.capitalize())
+
+#Title
+name="Welcome to python programming"
+print("Title case:",name.title())
